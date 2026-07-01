7a0a89c68509cf3bb78ae74ca653c2dadcaa0510
Saksit37 <saksit08102548@gmail.com>
2026-07-01 10:55:11 +0700

    add exercise

--- Lab2_exercise4.dart
+++ Lab2_exercise4.dart
@@ -3,6 +3,7 @@ void main(){
   String city = "Nakorn Sawan"; //ประกาศตัวแปร (non-nullable)
 
   int middleNameLength = middleName?.length ?? 0; //พิมพ์ความยาวของ middleName ถ้า middleName เป็น null ให้ใช้ค่า 0 แทน
+  print(" ");
   print("Middle name length : $middleNameLength");
 
   print("City length : ${city.length}"); //พิมพ์ความยาวของ city
