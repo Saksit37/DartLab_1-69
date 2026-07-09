cc19d254e4b22bd622637b4f749b3be8ad33b956
Saksit37 <saksit08102548@gmail.com>
2026-07-10 01:06:05 +0700

    update

--- ex2_1_2_temperature.dart
+++ ex2_1_2_temperature.dart
@@ -1,13 +1,42 @@
 class Temperature {
-   double _celsius;
+  double _celsius;
 
-   Temperature(this._celsius);
+  Temperature(this._celsius);
 
-   double toFahrenheit() {
-     return (_celsius * 9 / 5) + 32;
-   }
+  double get celsius => _celsius;
 
-   double toKelvin() {
-     return _celsius + 273.15;
-   }
+  set celsius(double value){
+    if (value >= -273.15) {
+      _celsius = value;
+    } else {
+      print('อุณหภูมิจะต่ำกว่า -273.15(ศูนย์สัมบูรณ์) องศาเซลเซียสไม่ได้');
+    }
+  }
+
+  double get fahrenheit => (_celsius * 9 / 5) + 32;
+
+}
+
+void main() {
+  Temperature temp = Temperature(25.0);
+
+  print("\nอุณหภูมิเริ่มต้น");
+  print("------------------------------");
+  print("Celsius      : ${temp.celsius} °C");
+  print("Fahrenheit   : ${temp.fahrenheit} °F");
+
+  print("\n================================");
+  print("ลองทดสอบอุณหภูมิที่ต่ำกว่า -273.15 °C");
+  print("================================");
+  print("ให้เป็น         : -300.0 °C");
+
+  temp.celsius = -300.0;
+
+  print("\n------------------------------");
+  print("อุณหภูมิปัจจุบัน");
+  print("------------------------------");
+  print("Celsius      : ${temp.celsius} °C");
+  print("Fahrenheit   : ${temp.fahrenheit} °F");
+
+  print("\n================================");
 }
